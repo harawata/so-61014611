5956d92d5cade5dbcc3a09397c7e5205421a8cad
Iwao AVE! <harawata@gmail.com>
2020-04-04 04:17:00 +0900

    Removed unrelated `@Repository` annotation

--- src/main/java/com/mybatis/dao/UserDao.java
+++ src/main/java/com/mybatis/dao/UserDao.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
-@Repository("userDao")
 @Mapper
 /*
  * 使用Spring自动扫描MyBatis的接口并装配 （Spring将指定包中所有被@Mapper注解标注的接口自动装配为MyBatis的映射接口
